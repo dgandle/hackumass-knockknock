0b06ef7a2c9f30ef6bb23ee662fa5a40e51faa33
Doug Gandle <dgandle@umass.edu>
2016-10-09 03:49:48 +0000

    more shit

--- Web Server/app.py
+++ Web Server/app.py
@@ -6,33 +6,38 @@ import time
 import subprocess
 
 
+
 app = Flask(__name__)
 
-
-
 @app.route('/')
+
 def index():
     templateData = {
         'image' : '/static/image.jpg',
-        'route' : '/opening'
+        'route' : '/opening',
+        'button-text' : 'unlock'
     }
     return render_template('index.html', **templateData)
 
 @app.route("/opening")
+
 def runmotor():
-    subprocess.call("sudo python static/servo3.py", shell = True)
+    subprocess.call("sudo python static/servo4.py", shell = True)
     templateData = {
         'image' : '/static/image.jpg',
-        'route' : '/closing'
+        'route' : '/closing',
+        'button-text' : 'unlock'
     }
     return render_template('index.html', **templateData)
 
 @app.route("/closing")
+
 def closemotor():
-    subprocess.call("sudo python static/servo4.py", shell = True)
+    subprocess.call("sudo python static/servo3.py", shell = True)
     templateData = {
         'image' : '/static/image.jpg',
-        'route' : '/opening'
+        'route' : '/opening',
+        'button-text' : 'lock'
     }
     return render_template('index.html', **templateData)
 
---
+++ clowndet.py
@@ -0,0 +1,11 @@
+from clarifai import rest
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+app = ClarifaiApp("INUyp_yCjazrOaEHiOYUuj2z9IMn8NI02QLMtYpQ","o6lCyQZYokg84eRY-JzsdP3UK6MhOOyzXaUz2E-T")
+
+url = 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Clouds_over_the_Atlantic_Ocean.jpg'
+model = app.models.get('general-v1.3')
+image = ClImage(url=url)
+app._parse_response(model.predict([image]))
+
+
